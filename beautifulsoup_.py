369d7bf5b1e5bcc6468d3e3c11a4eaaeac6e8605
yunuskaraman <34096246+yunuskaraman@users.noreply.github.com>
2020-11-18 01:42:14 +0300

    Add files via upload

---
+++ beautifulsoup_.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+url="https://www.imdb.com/chart/top/?ref_=nv_mv_250"
+
+html = requests.get(url).content
+soup = BeautifulSoup(html , "html.parser")
+
+list = soup.find("tbody", {"class":"lister-list"}).find_all("tr")
+
+count=1
+
+for tr in list:
+    title = tr.find("td", {"class":"titleColumn"}).find("a").text
+    year = tr.find("td", {"class":"titleColumn"}).find("span").text.strip("()")
+    reyting=tr.find("td", {"class":"ratingColumn imdbRating"}).find("strong").text
+    print(f"{count}:filmin adi : {title.ljust(30)} filmin yılı : {year} film değerlendirmesi : {reyting}")
+    count+=1
